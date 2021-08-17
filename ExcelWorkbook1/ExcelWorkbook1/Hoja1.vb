c441dacb2a995dda9b7c8a619e8af6427c8c6983
elbuendanylo <89085675+elbuendanylo@users.noreply.github.com>
2021-08-17 12:50:31 -0500

    Add files via upload

---
+++ ExcelWorkbook1/ExcelWorkbook1/Hoja1.vb
@@ -0,0 +1,15 @@
+﻿
+Public Class Hoja1
+    Dim frm As New frmMO
+    Private Sub Hoja1_Startup() Handles Me.Startup
+
+    End Sub
+
+    Private Sub Hoja1_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        frm.ShowDialog()
+    End Sub
+End Class
